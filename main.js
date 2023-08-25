402e6ee520e1d7cfa53ead431773922f2de28559
jvillacinda <jvillacoding@gmail.com>
2023-08-25 16:34:32 -0700

    intial push

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function generateNewLineTemplate () {
+
+    let i = document.getElementById("intial-year").value;
+    let f= document.getElementById("final-year").value;
+    let make = document.getElementById("make").value;
+    let model = document.getElementById("model").value;
+    let trim = document.getElementById("trim").value;
+    let engine = document.getElementById("engine").value;
+
+    console.log(i);
+    console.log(f);
+    console.log(make);
+    console.log(model);
+    console.log(trim);
+    console.log(engine);
+
+    // for (let index = 0; index < array.length; index++) {
+    //     const element = array[index];
+        
+    // }
+}
+
+

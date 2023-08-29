fc7e7588408d9abe0a78a60d5258248f86388dfb
jvillacinda <jvillacoding@gmail.com>
2023-08-29 13:22:14 -0700

    works

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
+function generateTemplate(event) { 
 
-function generateNewLineTemplate () {
-
-    let i = document.getElementById("intial-year").value;
+    let i = document.getElementById("initial-year").value;
     let f= document.getElementById("final-year").value;
     let make = document.getElementById("make").value;
     let model = document.getElementById("model").value;
     let trim = document.getElementById("trim").value;
     let engine = document.getElementById("engine").value;
+    
+    let outputDiv = document.getElementById("output");
 
-    console.log(i);
-    console.log(f);
-    console.log(make);
-    console.log(model);
-    console.log(trim);
-    console.log(engine);
-
-    // for (let index = 0; index < array.length; index++) {
-    //     const element = array[index];
-        
-    // }
+    for (let index = i; index <= f; index++) {
+        let template = `${index}>${make}>${model}>${trim}>${engine}<br>`;
+        outputDiv.innerHTML+=template;
+    }
+    
 }
 
-
+function clearList(){
+    let outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = ""; 
+}

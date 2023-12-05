751d4f6f9841e54d815bc3eaffa37a69ffcc8dae
jvillacinda <jvillacoding@gmail.com>
2023-12-05 13:03:11 -0800

    submit on enter. final must be greater than initial

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 function generateTemplate(event) { 
 
+    event.preventDefault();
+
     let i = document.getElementById("initial-year").value;
     let f= document.getElementById("final-year").value;
-    let make = document.getElementById("make").value;
-    let model = document.getElementById("model").value;
+    let make = document.getElementById("Make").value;
+    let model = document.getElementById("Model").value;
     let trim = document.getElementById("trim").value;
-    let engine = document.getElementById("engine").value;
+    let engine = document.getElementById("engine-name").value;
     
     let outputDiv = document.getElementById("output");
-
-    for (let index = i; index <= f; index++) {
-        let template = `${index}>${make}>${model}>${trim}>${engine}<br>`;
-        outputDiv.innerHTML+=template;
+    if (i<=f) {
+        for (let index = i; index <= f; index++) {
+            let template = `${index}>${make}>${model}>${trim}>${engine}<br>`;
+            outputDiv.innerHTML+=template;
+        }
+    } else {
+        alert("Final year must greater than or equal to intial year!")        
     }
     
 }

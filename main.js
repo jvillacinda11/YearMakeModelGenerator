6a8084a839c2e26df7ec571feb05c444886647f7
jvillacinda <jvillacoding@gmail.com>
2024-10-23 11:55:14 -0700

    end of line option

--- main.js
+++ main.js
@@ -8,18 +8,26 @@ function generateTemplate(event) {
     let model = document.getElementById("Model").value;
     let trim = document.getElementById("trim").value;
     let engine = document.getElementById("engine-name").value;
+    let eol = document.querySelector('input[name="eol"]:checked').value;
+
+    console.log(eol)
     
     let outputDiv = document.getElementById("output");
     if (i<=f) {
         for (let index = i; index <= f; index++) {
+                if (eol === 'nl') {
+                    let template = `${index}>${make}>${model}>${trim}>${engine}\r\n`;
+                    outputDiv.innerHTML+=template;
+                }
+                if (eol === 'vert') {
+                    let template = `${index}>${make}>${model}>${trim}>${engine}|`;
+                    outputDiv.innerHTML+=template;
+                }
 
-
-                let template = `${index}>${make}>${model}>${trim}>${engine}\r\n`;
-                outputDiv.innerHTML+=template;
             }
 
     } else {
-        alert("Final year must greater than or equal to intial year!")        
+        alert("Final year must greater than or equal to initial year!")        
     }
     
 }
